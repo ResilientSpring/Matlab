c824c007984fc440bca73523e56742e760d4921e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-19 14:52:11 +0800

    Committed 2022/05/19

--- 3D_cone_filter.m
+++ 3D_cone_filter.m
@@ -13,3 +13,25 @@ wt = 0.2 * pi;
 Nsamp = 200;
 %
 %
+theta_w = theta * pi / 180;
+
+if theta >= 45
+    wup = pi;
+else
+    wup = pi * tan(theta_w);
+end
+
+deltaw_wup = wup / Nsamp;
+deltaw_pi = pi / Nsamp;
+deltaw_pih = 0.5 * pi / Nsamp;
+%
+%
+r = zeros(3, 1);
+Q = zeros(3, 3);
+for iw = 0:Nsamp
+    w2 = iw * deltaw_wup;
+    w1 = w2 / tan(theta_w);
+    c = [1; -1-cos(w1)*cos(w2); -cos(w1)-cos(w2)];
+    r = r - 2 * cos(w1) * c;
+    Q = Q + c * c';
+end
