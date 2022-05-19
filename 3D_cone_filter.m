dd61e0128549dbc33f0cceea85141fef3f5f2147
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-19 15:05:13 +0800

    Committed 2022/05/19

--- 3D_cone_filter.m
+++ 3D_cone_filter.m
@@ -35,3 +35,39 @@ for iw = 0:Nsamp
     r = r - 2 * cos(w1) * c;
     Q = Q + c * c';
 end
+wc = acos(T(1));
+t11 = T(2);
+t01 = T(3);
+t00 = t11;
+t10 = 1 + t01;
+%
+% Design of prototype FIR lowpass filter
+%
+N_h = 2 * N + 1;
+wp = wc;
+
+ws = wp + wt;
+%
+%
+NH = (N_h - 1) / 2;
+Nsam_p = round(wp / deltaw_pi);
+NV = (0 :NH)';
+%
+%
+P = zeros(NH+1, 1);
+Qp = zeros(NH+1, NH+1);
+for iw = 0:Nsam_p
+    w = iw * deltaw_pi;
+    P = P - 2 * cos(w * NV);
+    Qp = Qp + cos(w * NV) * (cos(w * NV))';
+end
+
+P = wp * P / (Nsam_p + 1);
+Qp = wp * Qp / (Nsam_p + 1);
+%
+Qs = zeros(NH+1, NH+1);
+for iw = 0:Nsam_s
+    w = ws + iw * deltaw_pi;
+    Qs = Qs + cos(w * NV) * (cos(w * NV))';
+    
+end
