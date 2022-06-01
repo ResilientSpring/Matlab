0e396ae70fc8763dafcf26b71c7123c1196588d3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-01 18:35:05 +0800

    Committed 2022/06/01

--- Fractional-delay_FIR_digital_filters.m
+++ Fractional-delay_FIR_digital_filters.m
@@ -51,4 +51,52 @@ Qa = wp * Qa / point;
 a = -0.5 * inv(Qa) * ra;
 %
 %
+rb = zeros(nmb, 1);
+Qb = zeros(nmb, nmb);
 
+for iw = 0:pointw
+    w = iw * deltaw;
+
+    for ip = 0:pointp
+        s = zeros(nmb, 1);
+        for is = 0:nmb - 1
+            n = mod(is, NH) + 1;
+            m = floor(is/NH) + 1;
+            s(is+1) = p^(2*m-1)*sin(n*w);
+        end
+        rb = rb + 2*sin(p * w) * s;
+        Qb = Qb + s * s';
+    end
+end
+rb = wp * rb / point;
+Qb = wp * Qb / point;
+b = - 0.5 * inv(Qb)*rb;
+%
+%
+a2 = reshape(a, NH+1, Mc);
+b2 = reshape(b, NH, Ms);
+h = zeros(N+1, M+1);
+h(NH+1, 1) = 1;
+for im = 1:Mc
+    h(NH+1, 2 * im + 1) = a2(1, im);
+    h(1 : NH, 2 * im + 1) = 0.5 * a2(NH+1:-1:2, im);
+    h(NH + 2 : NH + 1, 2 * im + 1) = 0.5 * a2(2 : NH + 1, im);
+end
+for im = 1:Ms
+    h(1:NH, 2 * im) = 0.5 * b2(NH:-1:1, im);
+    h(NH+2:N+1, 2 * im) = -0.5 * b2(1:NH, im);
+end
+%
+%
+MR = zeros(pointw+1, pointp+1);
+GD = zeros(pointw+1, pointp+1);
+
+for ip = 0:pointp
+    p = -0.5 + ip * deltap;
+    hnp = h(:, 1);
+
+    for im = 1:M
+        hnp = hnp + h(:, im+1) * p ^ im;
+    end
+    
+end
