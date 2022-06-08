3d437fac7430ec69031c0a09a02ee88a6e3523f3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-08 23:45:43 +0800

    Committed 2022/06/08

--- VFO.m
+++ VFO.m
@@ -51,4 +51,44 @@ for ip = 0:pointp
 end
 ra = (p2 - p1) * (w2 - w1) * ra / point;
 Qa = (p2 - p1) * (w2 - w1) * Qa / point;
+rb = (p2 - p1) * (w2 - w1) * rb / point;
+Qb = (p2 - p1) * (w2 - w1) * Qb / point;
+%
+%
+a = - 0.5 * inv(Qa) * ra;
+a2 = reshape(a, NH+1, M+1);
+b = - 0.5 * inv(Qb) * rb;
+b2 = reshape(b, NH, M+1);
+%
+he = zeros(N+1, M+1);
+he(NH+1, :) = a2(1, :);
+he(NH+2:NH+1, :) = 0.5 * a2(2:NH+1, :);
+he(NH:-1:1, :) = he(NH+2:NH+1, :);
+%
+ho = zeros(N+1, M+1);
+ho(NH+2:N+1, :) = -0.5 * b2(1:NH, :);
+ho(NH:-1:1, :) = -ho(NH+2:NH+1, :);
+%
+h = he + ho;
+%
+MAG = zeros(pointw+1, pointp+1);
+XX = zeros(pointw+1, pointp+1);
+YY = zeros(pointw+1, pointp+1);
+for ip = 0:pointp
+    p = p1 + ip * deltap;
+    hnp = h(:, 1);
+    for im = 1:M
+        hnp = hnp + p^im*h(:, im+1);
+    end
+    MAG(:, ip+1) = abs(freqz(hnp, 1, w1:deltaw:w2));
+    for iw = 0:pointw
+        w = w1 + iw * deltaw;
+        XX(iw+1, ip+1) = w / pi;
+        YY(iw+1, ip+1) = p;
+    end
+end
 
+plot3(XX, YY, MAG);
+xlabel('Normalized frequency');
+ylabel('Variable p');
+zlabel('Variable magnitude response');
