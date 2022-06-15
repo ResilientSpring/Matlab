bb95bde5c91c5fc9c15217ad9133ff19a72049d9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-15 16:45:58 +0800

    Committed 2022/06/15

--- variable_LP_FIR.m
+++ variable_LP_FIR.m
@@ -21,3 +21,17 @@ deltap = 1 / sampling_p;
 sampling_pass = 0;
 sampling_stop = 0;
 ra = zeros(nma, 1);
+Qp = zeros(nma, nma);
+Qs = zeros(nma, nma);
+for iw = 0:sampling_w
+    w = iw * deltaw;
+    for ip = 0:sampling_p
+        p = -0.5 + ip * deltap;
+        cwp = zeros(nma, 1);
+        for inm = 0:nma-1
+            n = mod(inm, NH+1);
+            m = floor(inm/(NH+1));
+            cwp(inm+1) = p^(m)*cos(n*w);
+        end
+    end
+end
